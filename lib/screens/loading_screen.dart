3e85d8e642b7a0865a1cd1567a5e8093f3f73a67
Abiola Adebanjo <abiola.adebanjo@andela.com>
2019-06-17 02:22:57 +0100

    Stateful Widget Lifecycle Methods

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -15,16 +15,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 }
